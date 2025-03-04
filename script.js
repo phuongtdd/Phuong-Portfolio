98ff571aaf36fe11cc26c703f3873959df1c5db2
phuongtddd <phuong3dong@gmail.com>
2025-03-04 14:47:52 +0700

    Cap nhap avatar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-    // Open Menu DropDown
+// Open Menu DropDown
     function hamburg() {
         document.querySelector(".dropdown").classList.add("active");
     }
@@ -9,13 +9,20 @@
     }
 
     function contactMe() {
-        const email = "phuongtdd3711@gmail.com";
-        const subject = encodeURIComponent("Contact");
-        const body = encodeURIComponent("Hi, I would like to talk with you");
-        const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`;
-        window.open(gmailUrl, "_blank");
+        alert("Oops, I haven't uploaded my CV yet. Please feel free to contact me for more information. Thank you!")
+        //CV Download
+        // const email = "phuongtdd3711@gmail.com";
+        // const subject = encodeURIComponent("Contact");
+        // const body = encodeURIComponent("Hi, I would like to talk with you");
+        // const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`;
+        // window.open(gmailUrl, "_blank");
     }
 
     function downloadCv() {
-        alert("Oops, I haven't uploaded my CV yet. Please feel free to contact me for more information. Thank you!")
+        const link = document.createElement('a');
+        link.href = 'image/CV/MyCV.pdf'; // Replace with the actual path to your file
+        link.download = 'Phuong_CV.pdf'; // Replace with the desired file name
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     }
