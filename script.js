c30d28ecfe753a3f3519258cee3164c8ccf03c21
phuongtddd <phuong3dong@gmail.com>
2025-03-06 22:01:12 +0700

    update JS

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 // Open Menu DropDown
-    function hamburg() {
-        document.querySelector(".dropdown").classList.add("active");
-    }
+function hamburg() {
+    document.querySelector(".dropdown").classList.add("active");
+}
 
-    // Hide Menu DropDown
-    function cancel() {
-        document.querySelector(".dropdown").classList.remove("active");
-    }
+// Hide Menu DropDown
+function cancel() {
+    document.querySelector(".dropdown").classList.remove("active");
+}
 
-    function contactMe() {
-        alert("Oops, I haven't uploaded my CV yet. Please feel free to contact me for more information. Thank you!")
-        //CV Download
-        // const email = "phuongtdd3711@gmail.com";
-        // const subject = encodeURIComponent("Contact");
-        // const body = encodeURIComponent("Hi, I would like to talk with you");
-        // const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`;
-        // window.open(gmailUrl, "_blank");
-    }
+function contactMe() {
+    //CV Download
+    const email = "phuongtdd3711@gmail.com";
+    const subject = encodeURIComponent("Contact");
+    const body = encodeURIComponent("Hi, I would like to talk with you");
+    const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`;
+    window.open(gmailUrl, "_blank");
+}
 
-    function downloadCv() {
-        const link = document.createElement('a');
-        link.href = 'image/CV/MyCV.pdf'; // Replace with the actual path to your file
-        link.download = 'Phuong_CV.pdf'; // Replace with the desired file name
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
+function downloadCv() {
+    // const link = document.createElement('a');
+    // link.href = 'image/CV/MyCV.pdf'; // Replace with the actual path to your file
+    // link.download = 'Phuong_CV.pdf'; // Replace with the desired file name
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
+    alert("Oops, I haven't uploaded my CV yet. Please feel free to contact me for more information. Thank you!")
+
+}
